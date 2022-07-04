b5c300e4a40d82aacfa8a1c3711f52d59b975779
ngoducdo <ngo2682@gmail.com>
2022-07-04 17:04:45 +0700

    header

--- react-book/src/bookComponents/FooterBook.js
+++ react-book/src/bookComponents/FooterBook.js
@@ -1,68 +1,159 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
 
 const FooterBook = () => {
-    return (
-        <section className="">
-            <footer className=" text-white text-center text-md-start" style={{background:"#898AA6"}}>
-                <div className="container p-4">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Footer Content</h5>
-
-                            <p>
-                                Footer ContentFooter ContentFooter ContentFooter ContentFooter Content
-                                Footer ContentFooter ContentFooter ContentFooter ContentFooter Content
-                                Footer ContentFooter ContentFooter ContentFooter ContentFooter Content
-                            </p>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Links</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="/" className="text-white">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase mb-0">Links</h5>
-
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="/" className="text-white">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="/" className="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
+  return (
+    <section>
+      <div class="ma-footer-static">
+        <div class="footer-static">
+          <div class="container">
+            <div class="container-inner">
+              <div class="row hidden-xs">
+                <div class="col-md-4">
+                  <div>
+                    <div>
+                      <div class="col-sm-12 col-md-12 col-xs-12 address-footer">
+                        Lầu 5, 387-389 Hai Bà Trưng Quận 3 TP HCM
+                      </div>
+                      <div class="col-sm-12 col-md-12 col-xs-12 address-footer">
+                        Công Ty Cổ Phần Phát Hành Sách TP HCM - FAHASA
+                      </div>
+                      <div class="col-sm-12 col-md-12 col-xs-12 address-footer">
+                        60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam
+                      </div>
+                      <div class="col-sm-12 col-md-12 col-xs-12 address-footer1">
+                        Fahasa.com nhận đặt hàng trực tuyến và giao hàng tận
+                        nơi. KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn
+                        phòng cũng như tất cả Hệ Thống Fahasa trên toàn quốc.
+                      </div>
                     </div>
+                  </div>
                 </div>
-
-                <div className="text-center p-3" style={{}}>
-                    © 2022 Footer Content :
-                    <a className="text-white" href="https://mdbootstrap.com/"> Footer Content </a>
+                <div class="col-md-8">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-4 col-sms-6 col-smb-12">
+                      <div class="footer-static-title">
+                        <h3>DỊCH VỤ</h3>
+                      </div>
+                      <div class="footer-static-content">
+                        <ul>
+                          <li class="first">
+                            <a href="https://www.fahasa.com/dieu-khoan-su-dung">
+                              Điều khoản sử dụng
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/chinh-sach-bao-mat">
+                              Ch&iacute;nh s&aacute;ch bảo mật
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/chinh-sach-bao-mat-thanh-toan">
+                              Ch&iacute;nh s&aacute;ch bảo mật thanh toán
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/gioi-thieu-fahasa">
+                              Giới thiệu Fahasa
+                            </a>
+                          </li>
+                          <li class="last">
+                            <a href="https://www.fahasa.com/he-thong-trung-tam-nha-sach">
+                              Hệ thống trung t&acirc;m - nh&agrave; s&aacute;ch
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-sms-6 col-smb-12">
+                      <div class="footer-static-title">
+                        <h3>Hỗ trợ</h3>
+                      </div>
+                      <div class="footer-static-content ">
+                        <ul>
+                          <li class="first">
+                            <a href="https://www.fahasa.com/doi-tra-hang">
+                              Chính sách đổi - trả - hoàn tiền
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/chinh-sach-khach-si">
+                              Chính sách khách sỉ
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/phuong-thuc-van-chuyen">
+                              Phương thức vận chuyển
+                            </a>
+                          </li>
+                          <li class="last">
+                            <a href="https://www.fahasa.com/huong-dan-thanh-toan-va-xuat-hd">
+                              Phương thức thanh to&aacute;n và xuất HĐ
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-sms-6 col-smb-12">
+                      <div class="footer-static-title">
+                        <h3>T&agrave;i khoản của t&ocirc;i</h3>
+                      </div>
+                      <div class="footer-static-content">
+                        <ul>
+                          <li class="first footer_login_link">
+                            <a href="https://www.fahasa.com/index.php/customer/account/login">
+                              Đăng nhập/Tạo mới t&agrave;i khoản
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/index.php/customer/address">
+                              Thay đổi địa chỉ kh&aacute;ch h&agrave;ng
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.fahasa.com/index.php/customer/account">
+                              Chi tiết t&agrave;i khoản
+                            </a>
+                          </li>
+                          <li class="last">
+                            <a href="https://www.fahasa.com/index.php/sales/order/history">
+                              Lịch sử mua h&agrave;ng
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="footer-static-title">
+                    <h3>Li&ecirc;n hệ</h3>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <em class="fa fa-map-marker">&nbsp;</em>
+                      60-62 Lê Lợi, Q.1, TP. HCM
+                    </div>
+                    <div class="col-md-4">
+                      <em class="fa fa-envelope">&nbsp;</em>
+                      cskh@fahasa.com.vn
+                    </div>
+                    <div class="col-md-4">
+                      <em class="fa fa-phone">&nbsp;</em>
+                      1900636467
+                    </div>
+                  </div>
                 </div>
-            </footer>
-        </section>
-    );
+              </div>
+            </div>
+            <div class="ma-footer">
+              <div class="footer">
+                <div id="back-top" class="hidden-xs"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FooterBook;
--- react-book/src/bookComponents/MenuItem.js
+++ react-book/src/bookComponents/MenuItem.js
@@ -23,16 +23,141 @@ const MenuItem = () => {
         });
     }
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav" className="text-center">
-                    <Nav className="me-auto">
-                            {listCategory}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div class="row cms-icon-menu">
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_FlashSale_Hot_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">Flash Sale</div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_MaGiamGia_Hot_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">Mã Giảm Giá</div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_Trending_Moi_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">Xu Hướng</div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_PhienChoCu_Hot.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">
+            Phiên Chợ <br />
+            Sách Cũ
+          </div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_SanPhamMoi_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">
+            Sản Phẩm <br />
+            Mới
+          </div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/Duy-VHDT/DoChoi/Thang5/Icon_DoChoi_Hot.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">
+            Đồ Chơi <br />
+            Giáo Dục
+          </div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_VanPhongPham_Hot_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">
+            Văn Phòng Phẩm
+          </div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_ThieuNhi_Moi_1.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">Thiếu Nhi</div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_MangaCommic.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">
+            Manga - Comic
+          </div>
+        </div>
+        <div class="col-sms-3 col-sm-3 col-md-3">
+          <div>
+            <a href="#">
+              <img
+                src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/Icon_KinhTe.png"
+                class="center"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="cms-icon-menu-item-name">Kinh Tế</div>
+        </div>
+      </div>
     );
 };
 
