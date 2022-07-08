533d775a054f6d8c0b3fd78f94e12532ed6c01b2
nguyenlq2507 <nguyenlq2507@gmail.com>
2022-07-08 14:55:14 +0700

    nguyen14

--- react-book/src/bookComponents/Admin/ChartItem/ChartMonth.js
+++ react-book/src/bookComponents/Admin/ChartItem/ChartMonth.js
@@ -67,7 +67,7 @@ const ChartMonth = () => {
     return (
         <div>
             <AreaChart width={800} height={250} data={data}
-                       margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                 <defs>
                     <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
