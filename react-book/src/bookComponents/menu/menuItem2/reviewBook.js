c12ab7adb9af622e796496d97af3290b165c5771
ngoducdo <ngo2682@gmail.com>
2022-07-08 11:05:25 +0700

    menuitem5

--- react-book/src/bookComponents/menu/menuItem2/reviewBook.js
+++ react-book/src/bookComponents/menu/menuItem2/reviewBook.js
@@ -2,11 +2,107 @@ import React from 'react';
 
 const ReviewBook = () => {
     return (
-        <div>
-            <h1>review book</h1>
-            <h1>review book</h1>
-            <h1>review book</h1>
-            <h1>review book</h1>
+        <div class="main-container col1-layout">
+            <div class="main"  style={{ backgroundColor: "#eaa4a4" }}>
+                <div class="page-empty-banner col-lg-12 col-md-12 col-sm-12">
+                    <div style={{textAlign: "center"}}>
+                        <div class="page-banner">
+                            <a  >
+                                <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Main banner.jpg" alt="" />
+                            </a>
+                        </div>
+            
+                    </div>
+                    <div class="col-main">
+                        <div class="container" style={{backgroundColor: "#eaa4a4"}}>
+                            <div class="container-inner">
+                                <div class="tabs-prod">
+                                    <div class="row">
+                                        <div >
+                                            <div class=" fhs-banner-image-block ">
+                                                <div class="col-xs-12">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="" >
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Banner_01.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+            
+                                    <div class="row">
+                                        <div >
+                                            <div class=" fhs-banner-image-block " style="">
+                                                <div class="col-xs-12">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="">
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Tagname chinh_01.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+            
+                                    <div class="row">
+                                        <div >
+                                            <div class=" fhs-banner-image-block " style="">
+                                                <div class="col-sm-3 col-xs-3 no-margin-right">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="">
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Icon_01.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-sm-3 col-xs-3 no-margin-right">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="">
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Icon_02.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-sm-3 col-xs-3 no-margin-right" >
+                                                    <div class="banner-home-inner ">
+                                                        <a class="" >
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Icon_03.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-sm-3 col-xs-3 no-margin-right">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="">
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Icon_04.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div  >
+                                            <div class=" fhs-banner-image-block " >
+                                                <div class="col-xs-12">
+                                                    <div class="banner-home-inner ">
+                                                        <a class="" >
+                                                            <img src="https://cdn0.fahasa.com/media/wysiwyg/Thang-07-2022/DDay_Banner_02.jpg" alt="" />
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
